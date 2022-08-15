13b84eac978d17c54c06600edf7c3cec35884513
Hasan Fatih Yıldırgan <fatihyildirgan@hotmail.com>
2022-08-15 20:33:46 +0300

    new info1

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ a = 5
 b = 10.5
 # complex örnek
 x = 2j + 1
-
+# Yazdırmak için her seferinde print kullanmamız gerekiyor.
+print(a)
 print(a * b)
+# Değişiklerle birlikte tutmak proje ilerledikçe sağlıklı ilerlememizi sağlar,
+# Tipleri değiştirmek
+# integer'ı float'a çevirmek için;
+print(int(b))
+print(float(a))
+print(a * b / 10)
+# İnteger formda bu şekilde kullanılır.
+print(int(a * b / 10))
